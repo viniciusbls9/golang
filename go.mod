7e10634dd2956a8d7fd3eee8e804ad39794cb5a7
viniciusbls9 <beneditoloura@gmail.com>
2023-07-12 08:19:30 -0300

    feat: using my own string library

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/viniciusbls9/hellogo
 
+replace github.com/viniciusbls9/mystring v0.0.0 => ../mystring
+
+require (
+  github.com/viniciusbls9/mystring v0.0.0
+)
+
 go 1.20
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/viniciusbls9/mystring"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(mystring.Reverse("hello world"))
 }
